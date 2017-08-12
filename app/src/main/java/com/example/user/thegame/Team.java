4a5a63ad178a21f2a2558b4dd5bb44569d8e2d0e
staceynapier <staceynapier86@gmail.com>
2017-08-12 15:31:32 +0100

    added Team Class with basic methods and tests

--- app/src/main/java/com/example/user/thegame/Team.java
+++ app/src/main/java/com/example/user/thegame/Team.java
@@ -5,4 +5,20 @@ package com.example.user.thegame;
  */
 
 public class Team {
+
+    private String name;
+    private Integer score;
+
+    public Team(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
---
+++ app/src/test/java/com/example/user/thegame/TeamTest.java
@@ -0,0 +1,30 @@
+package com.example.user.thegame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by user on 12/08/2017.
+ */
+
+public class TeamTest {
+
+    Team team;
+
+    @Before
+    public void before(){
+        team = new Team("TeamName");
+    }
+
+    @Test
+    public void hasTeamName(){
+        assertEquals("TeamName", team.getName());
+    }
+
+    @Test
+    public void hasTeamScore(){
+        assertEquals(0, team.getScore());
+    }
+}
