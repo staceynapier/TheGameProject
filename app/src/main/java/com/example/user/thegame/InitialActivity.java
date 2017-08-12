78888312cd6d991e8fcef015954ea945d1d391c3
staceynapier <staceynapier86@gmail.com>
2017-08-12 16:36:27 +0100

    added activity for initial screen and plaintext box

---
+++ app/src/main/java/com/example/user/thegame/InitialActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.thegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+    }
+}
