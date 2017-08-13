a2a6321d51bd26949c7f51ff7b7e3165bf3eaeef
staceynapier <staceynapier86@gmail.com>
2017-08-13 12:09:47 +0100

    added button to add new clue to list

--- app/src/main/java/com/example/user/thegame/InitialActivity.java
+++ app/src/main/java/com/example/user/thegame/InitialActivity.java
@@ -2,12 +2,43 @@ package com.example.user.thegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class InitialActivity extends AppCompatActivity {
+
+    EditText inputClueText;
+    Button inputButton;
+    Clue clue;
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        inputClueText = (EditText)findViewById(R.id.clue_input);
+        inputButton = (Button)findViewById(R.id.inputter);
     }
+
+    public void onInputButtonClicked(View button) {
+        Log.d(getClass().toString(), "onInputButtonClicked was called");
+        String clue = inputClueText.getText().toString();
+        Clue newClue = new Clue(clue);
+        game = new Game();
+        game.addClue(newClue);
+
+        Log.d(newClue.getName().toString(), "added to list");
+    }
+
+//    public void onClearButtonClicked(View button) {
+//        Button instruction = inputButton;
+//            instruction = game.empty();
+//        Log.d(game.getList().toString(), "list emptied");
+//    }
+
 }
