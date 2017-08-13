ecd06225ae76c526dc8bea19af17a72917d88759
staceynapier <staceynapier86@gmail.com>
2017-08-13 17:09:41 +0100

    added button and textview to gameActivity

--- app/src/main/java/com/example/user/thegame/GameActivity.java
+++ app/src/main/java/com/example/user/thegame/GameActivity.java
@@ -2,25 +2,38 @@ package com.example.user.thegame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+
+import static com.example.user.thegame.R.id.correct_button;
 
 public class GameActivity extends AppCompatActivity {
 
     TextView clueOutput;
     ArrayList<Clue> list;
     Game game;
+    Button correctButton;
+    Button passButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        correctButton = (Button)findViewById(R.id.correct_button);
+        passButton = (Button)findViewById(R.id.pass_button);
+
+    }
+
+    public void onCorrectButtonClicked(View button){
+
         clueOutput = (TextView)findViewById(R.id.clue_output);
 
     }
-//    game = new Game(list);
+    game = Game();
 //    String newClue = game.getRandomClue();
 //    clueOutput.setText(newClue);
 }
